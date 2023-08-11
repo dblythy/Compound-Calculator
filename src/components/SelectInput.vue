7f2415d58988d1e783068fc920c224eb5c14204e
dblythy <daniel-blyth@live.com.au>
2023-08-11 11:44:41 +1000

    run prettier

--- src/components/SelectInput.vue
+++ src/components/SelectInput.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="select-input">
-    <label :for="label">{{label}}</label>
+    <label :for="label">{{ label }}</label>
     <select :name="label" v-model="_value">
-    <option v-for="option of options" :value="option" :key="option">{{option}}</option>
-  </select>
+      <option v-for="option of options" :value="option" :key="option">
+        {{ option }}
+      </option>
+    </select>
   </div>
 </template>
 
@@ -18,28 +20,32 @@ export default {
   emits: ['update:modelValue'],
   computed: {
     _value: {
-      get () { return this.modelValue },
-      set (value) { this.$emit('update:modelValue', value) },
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
     },
   },
-}
+};
 </script>
 
 <style>
 .select-input {
-      display: flex;
-    align-items: center;
-    flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
 }
 .select-input label {
-    width: 100%;
-    text-align: left;
+  width: 100%;
+  text-align: left;
 }
 .select-input select {
   font-size: 14px;
-    background: #fff;
-    border: none;
-    outline: none;
-    padding: 0.5rem;
+  background: #fff;
+  border: none;
+  outline: none;
+  padding: 0.5rem;
 }
 </style>
