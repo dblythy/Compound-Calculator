a1f58a7885e7a6cb214a5cab6d1d5426605da5bf
dblythy <daniel-blyth@live.com.au>
2023-08-11 12:03:43 +1000

    prettier

--- src/components/__tests__/Calculator.spec.js
+++ src/components/__tests__/Calculator.spec.js
@@ -1,4 +1,4 @@
-import Calculator from '../Calculator.vue'
+import Calculator from '../Calculator.vue';
 
 describe('Calculator.vue Test', () => {
   it(`Does compute undefined when values aren't passed`, () => {
@@ -6,70 +6,72 @@ describe('Calculator.vue Test', () => {
   });
 
   const expected = [
-      {
-        options: {
-          interest: 1.1,
-          deposit: 10000,
-          term: 3,
-          termPeriod: 'months',
-          interestPeriod: 'Monthly',
-        },
-        output: 10027.525216035878
+    {
+      options: {
+        interest: 1.1,
+        deposit: 10000,
+        term: 3,
+        termPeriod: 'months',
+        interestPeriod: 'Monthly',
       },
-      {
-        options: {
-          interest: 2.1,
-          deposit: 10000,
-          term: 3,
-          termPeriod: 'months',
-          interestPeriod: 'Monthly',
-        },
-        output: 10052.591928593749
+      output: 10027.525216035878,
+    },
+    {
+      options: {
+        interest: 2.1,
+        deposit: 10000,
+        term: 3,
+        termPeriod: 'months',
+        interestPeriod: 'Monthly',
       },
-      {
-        options: {
-          interest: 1.1,
-          deposit: 20000,
-          term: 3,
-          termPeriod: 'months',
-          interestPeriod: 'Monthly',
-        },
-        output: 20055.050432071755
+      output: 10052.591928593749,
+    },
+    {
+      options: {
+        interest: 1.1,
+        deposit: 20000,
+        term: 3,
+        termPeriod: 'months',
+        interestPeriod: 'Monthly',
       },
-      {
-        options: {
-          interest: 1.1,
-          deposit: 10000,
-          term: 3,
-          termPeriod: 'years',
-          interestPeriod: 'Annually',
-        },
-        output: 10333.64331
+      output: 20055.050432071755,
+    },
+    {
+      options: {
+        interest: 1.1,
+        deposit: 10000,
+        term: 3,
+        termPeriod: 'years',
+        interestPeriod: 'Annually',
       },
-      {
-        options: {
-          interest: 1.1,
-          deposit: 10000,
-          term: 4,
-          termPeriod: 'months',
-          interestPeriod: 'At Maturity',
-        },
-        output: 10036.666666666666
+      output: 10333.64331,
+    },
+    {
+      options: {
+        interest: 1.1,
+        deposit: 10000,
+        term: 4,
+        termPeriod: 'months',
+        interestPeriod: 'At Maturity',
       },
-    ];
-    for (const {options, output} of expected) {
-  it(`Does correctly compute output ${output}`, () => {
+      output: 10036.666666666666,
+    },
+  ];
+  for (const { options, output } of expected) {
+    it(`Does correctly compute output ${output}`, () => {
       expect(Calculator.computed.balance.call(options).result).toBe(output);
     });
   }
 
   it(`Does correctly handle errors`, () => {
-    expect(Calculator.computed.balance.call({
-      interest: 1.1,
-      deposit: 10000,
-      term: 3,
-      termPeriod: 'months',
-      interestPeriod: 'Annually',
-    }).error).toBe("Annually must have a minimum of 12 months");
+    expect(
+      Calculator.computed.balance.call({
+        interest: 1.1,
+        deposit: 10000,
+        term: 3,
+        termPeriod: 'months',
+        interestPeriod: 'Annually',
+      }).error
+    ).toBe('Annually must have a minimum of 12 months');
   });
-})
+});
